331ebd7740b10ddcc3561791de5e5d80a42bbab6
jprio <jeanpierre.rio@gmail.com>
2022-04-09 09:22:10 +0200

    m

--- tests/unit/test_aws_cdk_stack.py
+++ tests/unit/test_aws_cdk_stack.py
@@ -1,15 +1,15 @@
-import aws_cdk as core
+import aws_cdk_labs as core
 import aws_cdk.assertions as assertions
 
-from aws_cdk_labs.aws_lambda_stack import AwsCdkLabsStack
+from aws_cdk_labs.aws_cdk_stack import AwsCdkStack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in aws_cdk_labs/aws_cdk_labs_stack.py
+# resource in aws_cdk/aws_cdk_stack.py
 
 
 def test_sqs_queue_created():
     app = core.App()
-    stack = AwsCdkLabsStack(app, "aws-cdk-labs")
+    stack = AwsCdkStack(app, "aws-cdk")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
